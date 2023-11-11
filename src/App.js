23932de0df22b2f7aa30fff653087c702664d3e4
kiran <spkiran.suresh@gmail.com>
2023-11-11 18:33:33 +0530

    working responsive header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Header from "./globalComponents/Header";
+const theme = createTheme({
+  // Override or create new styles, colors, palettes...
+  palette: {
+    primary: {
+      main: "#1b5c94",
+    },
+    icons: {
+      main: "#FFF",
+    },
+  },
+});
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <header>
+        <Header />
+      </header>
+    </ThemeProvider>
   );
 }
 
