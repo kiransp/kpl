5dff093a86d00d55df58df269374bae2619a623f
kiran <spkiran.suresh@gmail.com>
2023-11-19 16:00:05 +0530

    header and home for mobile. working.

--- src/Images.js
+++ src/Images.js
@@ -1,4 +1,6 @@
 export const kplLogo =
   "https://firebasestorage.googleapis.com/v0/b/whatsapp-61c82.appspot.com/o/kpl_logo_cropped.png?alt=media&token=d15be49f-9bec-4861-88ff-9f6528be974b";
-export const bannerImgae =
+export const bannerImage =
   "https://firebasestorage.googleapis.com/v0/b/kplcricket-d5078.appspot.com/o/hero_baner.png?alt=media&token=2794f782-fc7b-4997-b3dc-69a44cb81f41";
+export const bannerImageMobile =
+  "https://firebasestorage.googleapis.com/v0/b/kplcricket-d5078.appspot.com/o/hero_banner_mobile.png?alt=media&token=9c6e33ee-3bca-4450-a14a-ef186fc86b42";
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,39 @@
-import { Grid } from "@mui/material";
+import { Grid, useMediaQuery, useTheme } from "@mui/material";
 import styles from "./Home.module.scss";
-import { kplLogo } from "../../Images";
+import { bannerImage, bannerImageMobile, kplLogo } from "../../Images";
 
 const Home = () => {
+  const theme = useTheme();
+  const mobile = useMediaQuery(theme.breakpoints.down("sm"));
+  console.log("Mobile ", mobile);
   return (
-    <Grid container className={styles["home-container"]}>
-      <Grid item md={1}></Grid>
-      <Grid item md={11}>
-        <img src={kplLogo} alt="logo" className={styles["home-logo"]} />
-      </Grid>
+    <Grid
+      container
+      className={styles["home-container"]}
+      sx={{
+        backgroundImage: {
+          xs: `url(${bannerImageMobile})`,
+          sm: `url(${bannerImage})`,
+        },
+      }}
+    >
+      {mobile ? (
+        <Grid item className={styles["home-logo-img-container"]}>
+          <img
+            src={kplLogo}
+            alt="logo"
+            className={styles["home-logo-mobile"]}
+          />
+        </Grid>
+      ) : (
+        <>
+          {" "}
+          <Grid item md={1}></Grid>
+          <Grid item md={11}>
+            <img src={kplLogo} alt="logo" className={styles["home-logo"]} />
+          </Grid>
+        </>
+      )}
     </Grid>
   );
 };
