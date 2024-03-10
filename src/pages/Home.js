053cb151cf00b558b481d9ac9c69b4b200206b67
kiran <spkiran.suresh@gmail.com>
2024-03-11 00:20:36 +0530

    finals

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,13 +6,15 @@ import Prize from "../components/Prize/Prize";
 import Story from "../components/Story/Story";
 import TeamsCarousel from "../components/TeamsCarousel/TeamsCarousel";
 import Schedule from "../components/Schedule/Schedule";
-
+import Confetti from "react-confetti";
 const Home = () => {
   useEffect(() => {
     window.scroll(0, 0);
   }, []);
+
   return (
     <>
+      <Confetti width={window.innerWidth} height={2000} numberOfPieces={1000} />
       <HeroBanner />
       <About />
       <Prize />
