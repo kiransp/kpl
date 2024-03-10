cee64f0ba5f1d92256c71168c7929ebbfd58d7b5
kiran <spkiran.suresh@gmail.com>
2024-03-10 11:52:17 +0530

    new

--- src/sitedata/matches.js
+++ src/sitedata/matches.js
@@ -175,12 +175,12 @@ const matches = {
     {
       time: "9:00 AM - 10:30 AM",
       url1: "https://firebasestorage.googleapis.com/v0/b/kplcricket-d5078.appspot.com/o/teamDetails%2Ftop1.png?alt=media&token=8bd2a803-14c5-4953-b17e-f53e272f3a4d",
-      url2: "https://firebasestorage.googleapis.com/v0/b/kplcricket-d5078.appspot.com/o/teamDetails%2Ftop2.png?alt=media&token=509fd3b6-4e9d-42d7-8c82-8e1895f5a8d6",
+      url2: "https://firebasestorage.googleapis.com/v0/b/kplcricket-d5078.appspot.com/o/teamDetails%2Ftop4.png?alt=media&token=869e392e-26d5-40b6-9875-9761a21fc843",
     },
     {
       time: "10:30 AM - 12:00 PM",
-      url1: "https://firebasestorage.googleapis.com/v0/b/kplcricket-d5078.appspot.com/o/teamDetails%2Ftop3.png?alt=media&token=187559c1-8ae1-4f17-9975-b70c3e8e907f",
-      url2: "https://firebasestorage.googleapis.com/v0/b/kplcricket-d5078.appspot.com/o/teamDetails%2Ftop4.png?alt=media&token=869e392e-26d5-40b6-9875-9761a21fc843",
+      url1: "https://firebasestorage.googleapis.com/v0/b/kplcricket-d5078.appspot.com/o/teamDetails%2Ftop2.png?alt=media&token=509fd3b6-4e9d-42d7-8c82-8e1895f5a8d6",
+      url2: "https://firebasestorage.googleapis.com/v0/b/kplcricket-d5078.appspot.com/o/teamDetails%2Ftop3.png?alt=media&token=187559c1-8ae1-4f17-9975-b70c3e8e907f",
     },
   ],
   Finals: [
